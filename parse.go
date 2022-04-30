e07075185e05fd6287ee634259283f0083255531
l27001 <l27001@ezdomain.ru>
2022-04-30 22:50:48 +0400

    Парсер немного допилен

--- parse.go
+++ parse.go
@@ -1,6 +1,7 @@
 package main
 
 import ("fmt"
+	"errors"
 	"net/http"
 	"io"
 	"strings"
@@ -12,7 +13,7 @@ import ("fmt"
 
 var download_dir string = "/tmp/shawel-go/parse"
 
-func download_file(url string, to_disk bool) string {
+func download_file(url string, to_disk bool) (string, error) {
 	// Делаем http запрос
 	resp, err := http.Get(url)
 	if(err != nil) {
@@ -20,15 +21,14 @@ func download_file(url string, to_disk bool) string {
 	}
 	defer resp.Body.Close() // Освобождаем ресурсы после всех действий
 	if(resp.StatusCode != 200) {
-		fmt.Println(url+": "+resp.Status)
-		return ""
+		return "", errors.New(url+": "+resp.Status)
 	}
 	if(to_disk == false) {
 		bodyBytes, err := io.ReadAll(resp.Body) // Получаем body в виде байтов
 		if err != nil {
 			panic(err)
 		}
-		return string(bodyBytes) // Возвращаем body в виде строки
+		return string(bodyBytes), nil // Возвращаем body в виде строки
 	} else {
 		var file_split []string
 		var file string
@@ -43,20 +43,19 @@ func download_file(url string, to_disk bool) string {
 		if(err != nil) {
 			panic(err)
 		}
-		return file
+		return file, nil
 	}
 }
 
-func get_attr(body string, tag string, attr string) []string {
-	var iframes []string
+func get_attr(body string, tag string, attr string) [3]string {
+	iframes := [3]string{}
 	tag = "<"+tag+" "
 	attr = attr+"=\""
-	tags_count := strings.Count(body, tag)
-	for i := 0; i < tags_count; i++ {
+	for i := 0; i < len(iframes); i++ {
 		var params string
 		_, body, _ = strings.Cut(body, tag) // ищем нужный нам тег
 		params, _, _ = strings.Cut(body, ">") // получаем все между < и >
-		iframes = append(iframes, params) // добавляем тег в массив
+		iframes[i] = params // добавляем тег в массив
 		var link string
 		_, link, _ = strings.Cut(params, attr) // ищем нужный нам атрибут
 		link, _, _ = strings.Cut(link, "\"") // получаем его значение
@@ -96,6 +95,7 @@ func check_img_white_percent(file string) {
 func main() {
 	prefixes := map[int]string{
 		0: "rasp",
+		1: "test",
 		2: "zvonki",
 	}
 	err := os.MkdirAll(download_dir, 0750)
@@ -103,31 +103,37 @@ func main() {
 		panic(err)
 	}
 	defer os.RemoveAll(download_dir)
-	var body string
-	var links []string
-	body = download_file("https://engschool9.ru/content/raspisanie.html", false)
-	links = get_attr(body, "iframe", "src")
+	body, err := download_file("https://engschool9.ru/content/raspisanie.html", false)
+	if(err != nil) {
+		panic(err)
+	}
+	links := get_attr(body, "iframe", "src")
+	var filename string
 	for i, link := range links {
 		prefix, ok := prefixes[i]
-		if(ok == true) {
-			var filename string = download_file(link, true)
-			err := os.Mkdir(download_dir+"/"+prefix, 0750)
-			if(err != nil && !os.IsExist(err)) {
-				panic(err)
-			}
-			defer os.RemoveAll(download_dir+"/"+prefix)
-			cmd := exec.Command("convert", "-colorspace", "RGB", "-density", "200", download_dir+"/"+filename, download_dir+"/"+prefix+"/out.png")
-			_, err = cmd.Output()
-			if(err != nil) {
-				panic(err)
-			}
-			imgs, err := os.ReadDir(download_dir+"/"+prefix)
-			if(err != nil) {
-				panic(err)
-			}
-			for _, img := range imgs {
-				check_img_white_percent(download_dir+"/"+prefix+"/"+img.Name())
-			}
+		if(ok == false || link == "") {
+			continue
+		}
+		filename, err = download_file(link, true)
+		if(err != nil) {
+			fmt.Println(err)
+			continue
+		}
+		err := os.Mkdir(download_dir+"/"+prefix, 0750)
+		if(err != nil && !os.IsExist(err)) {
+			panic(err)
+		}
+		cmd := exec.Command("convert", "-colorspace", "RGB", "-density", "200", download_dir+"/"+filename, download_dir+"/"+prefix+"/out.png")
+		_, err = cmd.Output()
+		if(err != nil) {
+			panic(err)
+		}
+		imgs, err := os.ReadDir(download_dir+"/"+prefix)
+		if(err != nil) {
+			panic(err)
+		}
+		for _, img := range imgs {
+			check_img_white_percent(download_dir+"/"+prefix+"/"+img.Name())
 		}
 	}
 }
